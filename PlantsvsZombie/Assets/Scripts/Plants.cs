df8d5825111920e8d1fe449230797687a28c0563
wtsiwon <siwon4727@gmail.com>
2022-04-14 20:53:21 +0900

    저장

--- PlantsvsZombie/Assets/Scripts/Plants.cs
+++ PlantsvsZombie/Assets/Scripts/Plants.cs
@@ -2,14 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlantsType
+{
+
+}
 public abstract class Plants : MonoBehaviour
 {
+    Tile tile;
+
+    
     [Header("기본")]
     [SerializeField] protected float hp;
-    [SerializeField] protected Vector2 pos;
+    [SerializeField] protected int[,] pos = new int[5, 7];
 
     [Header("공격옵")]
     [SerializeField] protected float bulletInterval;
     
+
     
+}
+
+
+public struct Tile
+{
+    int x;
+    int y;
 }
